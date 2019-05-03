427d56eab5d81eb64f4a83c1a32fa71ce5fbf25c
Eric Tomasso <eric.tomasso@gmail.com>
2019-05-03 11:39:50 -0700

    Make MongoDB collection and 'email' key name configurable via .env file

--- server/lib/models/Subscriber.js
+++ server/lib/models/Subscriber.js
@@ -4,24 +4,27 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
+	/*
 	name: {
 		type: String,
 		required: false,
 		unique: false,
 		trim: false
 	},
-	email: {
+	*/
+	[process.env.MONGO_EMAIL_KEY]: {
 		type: String,
 		required: true,
 		unique: true,
 		trim: true
 	},
-
+	/*
 	joined_on: { 
 		type: Date, 
 		required: true, 
 		index: true 
 	},
+	*/
 })
 
-module.exports = mongoose.model(process.env.MONGO_COLLECTION_SINGULAR, schema);
+module.exports = mongoose.model(process.env.MONGO_COLLECTION_NAME_SINGULAR, schema);
