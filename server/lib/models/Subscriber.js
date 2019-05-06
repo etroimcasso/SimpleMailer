ccb36721786333a3267f0c97d958cfd18eef33a3
Eric Tomasso <eric.tomasso@gmail.com>
2019-05-06 11:39:12 -0700

    Fix issue where joined_on date not being stored

--- server/lib/models/Subscriber.js
+++ server/lib/models/Subscriber.js
@@ -4,27 +4,23 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const schema = new Schema({
-	/*
 	name: {
 		type: String,
 		required: false,
 		unique: false,
 		trim: false
 	},
-	*/
 	[(process.env.MONGO_EMAIL_KEY || "email")]: {
 		type: String,
 		required: true,
 		unique: true,
 		trim: true
 	},
-	/*
 	joined_on: { 
 		type: Date, 
 		required: true, 
 		index: true 
 	},
-	*/
 })
 
 module.exports = mongoose.model((process.env.MONGO_COLLECTION_NAME_SINGULAR || "Subscriber"), schema);
