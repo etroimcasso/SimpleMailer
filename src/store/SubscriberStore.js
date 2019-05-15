4af60924bc26fa9985d61d949742ddff6add0563
Eric Tomasso <eric@etroimcasso.xyz>
2019-05-14 19:06:15 -0700

    Fix issue where a number of observables were not marked properly in AppStateStore

--- src/store/SubscriberStore.js
+++ src/store/SubscriberStore.js
@@ -1,26 +1,52 @@
 import { decorate, observable, action} from "mobx"
 import openSocket from 'socket.io-client';
+import AppStateStore from '../../store/AppStateStore'
+const AppState = new AppStateStore()
 const hostname = require('../config/hostname.js');
 const socket = openSocket(hostname.opensocket);
+const ReconnectionTimer = require('./helpers/ReconnectionTimer');
+
+
+const getAllSubscribers = (callback) => {
+  socket.on('getAllSubscribersResult', (error, subscribers) => callback(error, subscribers))
+  socket.emit('getAllSubscribers')
+}
+
 
 export default class SubscriberStore {
 	subscribers = [] ///Rename all subscribersList props / states to this
+	subscribersLoaded = false
+	reloadSubscribersPending = false
+
+
+
 
    	constructor() {
+   		this.getAllSubscribers()
    		socket.on('connect') {
 
    		}
    	}
+	
+	//Reloads subscriber list
+	getAllSubscribers() {
+   		getAllSubscribers((error, subscribers) => {
+   			if (!error) 
+   				this.subscribers = JSON.parse(subscribers) 
+   		})
+   	}
+
 
 
 }
 
 export default decorate(SubscriberStore, {
 	subscribers: observable,
+	subscribersLoaded: observable,
+	reloadSubscribersPending: observable,
 	addSubscriber: action,
 	removeSubscriber: action,
 	getAllSubscribers: action,
-	fetchSubscribersFromServer: action
 
 })
 
